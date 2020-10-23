3637e280962218496735a914a390249acac7659d
Elijah Desktop <56444910+EaStory@users.noreply.github.com>
2020-10-23 10:46:32 -0400

    Finished the code for fading

--- Elijah-Story-Fading-LED/Elijah-Story-Fading-LED.ino
+++ Elijah-Story-Fading-LED/Elijah-Story-Fading-LED.ino
@@ -1,9 +1,41 @@
+//Elijah Story
+//10-23-2020
+//Lab7 Fading LED
+
+//Followed the video in the lab instructions.
+//Turns the light on and off with a fade.
+//Diagram of wiring in README.
+
+//Setting up the variables that will not be changed.
+const int buttonPin = 2;      //will get input from pin 2
+const int ledPin = 9;         //will output from pin 9
+const int fadingDelay = 50;   //the delay the led will fade in milliseconds
+
+
+int buttonState = 0;          //will say if the button is pressed or not
+boolean fadingState = false;  //says if the light is on or off
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);    //this will control the LEDs fade
+  pinMode(buttonPin, INPUT);  //this reads the button press
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin); //checks the state of the button
 
+  if(buttonState == HIGH){              //if the button is pressed
+    if(fadingState == false){           //if the light is off
+      for (int i = 0; i <= 255; i += 5){
+        analogWrite(ledPin, i);         //adds more power to LED based on loop
+        delay(fadingDelay);             //waits to give the fade affect
+      }
+    }else{                              //if the light is on
+      for (int i = 255; i >= 0; i -= 5){
+        analogWrite(ledPin, i);         //takes power from LED based on loop
+        delay(fadingDelay);             //waits to give the fade affect
+      }
+    }
+    fadingState = !fadingState;         //switch the state saying if the light is on or off.
+  }
 }

245aedb7f146313957d122255c63ee539629b89e
Elijah Desktop <56444910+EaStory@users.noreply.github.com>
2020-11-05 10:48:58 -0500

    Update Elijah-Story-RGB-Button.ino
    The code is done, but no comments

--- Elijah-Story-RGB-Button/Elijah-Story-RGB-Button.ino
+++ Elijah-Story-RGB-Button/Elijah-Story-RGB-Button.ino
@@ -22,7 +22,7 @@ int stepAdjust = 5;
 int buttonState = 0;          //will say if the button is pressed or not
 boolean fadingState = false;  //says if the light is changing
 
-float colors[][3] = {{255, 0, 0},{255, 127, 0},{255, 255, 0},{0, 255, 0},{0, 0, 255},{75, 0, 130},{143, 0, 255}};
+float colors[][3] = {{255, 0, 0}, {142, 68, 173}, {149, 165, 166}, {0, 255, 0}, {22, 188, 156}, {75, 0, 130}, {211, 84, 0}};
 
 
 void setup() {
@@ -33,44 +33,41 @@ void setup() {
 
   Serial.begin(9600);
 
-  for(int i = 0; i < 7; i++){
-    colors[i][0] = colors[i][0] / (255/stepAdjust);
-    colors[i][1] = colors[i][1] / (255/stepAdjust);
-    colors[i][2] = colors[i][2] / (255/stepAdjust);
-  }
-  setColor(redValue,greenValue,blueValue);
+//  for (int i = 0; i < 7; i++) {
+//    colors[i][0] = colors[i][0] / (255 / stepAdjust);
+//    colors[i][1] = colors[i][1] / (255 / stepAdjust);
+//    colors[i][2] = colors[i][2] / (255 / stepAdjust);
+//  }
+  setColor(redValue, greenValue, blueValue);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin); //checks the state of the button
 
-  if(buttonState == HIGH){              //if the button is pressed
-    if(!fadingState){
-        fadingState = true;
-        adjust(-1);
-        index++;
-        adjust(1);
-        Serial.print(redValue);
-        Serial.print(greenValue);
-        Serial.print(blueValue);
-        Serial.println("");
-        fadingState = false;
+  if (buttonState == HIGH) {            //if the button is pressed
+    if (!fadingState) {
+      fadingState = true;
+      adjust(-1);
+      index++;
+      adjust(1);
+      fadingState = false;
     }
   }
 }
 
-void adjust(int change){
-    for(int i = 0; i < 255; i += stepAdjust){
-        redValue += colors[index%(sizeof(colors)/sizeof(colors[0]))][0] * change;
-        greenValue += colors[index%(sizeof(colors)/sizeof(colors[0]))][1] * change;
-        blueValue += colors[index%(sizeof(colors)/sizeof(colors[0]))][2] * change;
-        setColor(redValue,greenValue,blueValue);
-        delay(fadingDelay);
-    }
+void adjust(int change) {
+  int sizeArray = (sizeof(colors) / sizeof(colors[0]));
+  for (int i = 0; i < 255; i += stepAdjust) {
+    redValue += colors[index % sizeArray][0] / (255 / stepAdjust) * change;
+    greenValue += colors[index % sizeArray][1] / (255 / stepAdjust) * change;
+    blueValue += colors[index % sizeArray][2] / (255 / stepAdjust) * change;
+    setColor(redValue, greenValue, blueValue);
+    delay(fadingDelay);
+  }
 }
 
 //this function sets the pin values base on its input
-void setColor(float red, float green, float blue){
+void setColor(float red, float green, float blue) {
   analogWrite(redPin, red);         //sets the red pin
   analogWrite(greenPin, green);     //sets the green pin
   analogWrite(bluePin, blue);       //sets the blue pin
